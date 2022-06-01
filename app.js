66f4b31c56bd15751d15b463e4e5da6f27e24b40
Dom Harbot <domharbot@gmail.com>
2022-06-01 11:02:27 +0100

    Update 1100.010622

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ class Cat extends Animal {
         this.thirst = thirst;
         this.malevolence = malevolence;
     }
+    feed() {
+        this.malevolence -= 10;
+        this.hunger -= 10;
+        this.thirst += 10;
+    }
+
+    giveWater() {
+        this.thirst -= 10;
+        this.hunger += 10;
+        this.malevolence -10;
+    }
+
+    ignore() {
+        this.thirst += 10;
+        this.hunger += 10;
+        this.malevolence += 10;
+    }
 }
 
 class Sloth extends Animal {
@@ -20,6 +37,23 @@ class Sloth extends Animal {
         this.speed = speed;
         this.chill = chill;
     }
+
+    feed(){
+        this.hunger -= 10;
+        this.speed -= 10;
+        this.chill += 10;
+    }
+
+    hangOut(){
+        this.hunger += 10;
+        this.speed -= 10;
+        this.chill += 10;
+    }
+
+    attachRocket(){
+        this.speed += 10;
+        this.chill -= 10;
+    }
 }
 
 class Dragon extends Animal {
@@ -27,6 +61,22 @@ class Dragon extends Animal {
         super(name, age, hunger);
         this.coal = coal;
         this.gaviscon = gaviscon;
+    }
+
+    feedKnight() {
+        this.hunger -=10;
+        this.coal += 10;
+        this.gaviscon += 10;
+    }
+
+    feedCoal() {
+        this.coal += 10;
+        this.gaviscon -= 10;
+    }
+
+    feedGaviscon() {
+        this.coal -= 10;
+        this.gaviscon += 10;
     }
 }
 
--- game.js
+++ game.js
@@ -1,8 +1,42 @@
 const inquirer = require("inquirer");
 const { Cat } = require("./app");
 const { Sloth } = require("./app");
+const { Dragon } = require("./app");
+let myPet = {};
 
-let myPet = "";
+const catFn = async () => {
+    const catAction = await inquirer.prompt({
+    type: "list",
+    name: "typeOfPet",
+    message: "What would you like to do with your new cat?",
+    choices: [
+        {
+            key: "a",
+            name: "Feed",
+            value: "feed",
+        },
+        {
+            key: "b",
+            name: "Give Water",
+            value: "giveWater",
+        },
+        {
+            key: "c",
+            name: "Ignore",
+            value: "ignore",
+        },
+    ],
+});
+if(catAction.typeOfPet == 'feed'){
+    await myPet.feed()
+}
+else if(catAction.typeOfPet == 'giveWater'){
+    await myPet.giveWater();
+}
+else if(catAction.typeOfPet == 'ignore'){
+    await myPet.ignore();
+}}
+
 
 try{
 
@@ -26,32 +60,40 @@ try{
                     key: "c",
                     name: "Dragon",
                     value: "dragon",
-                }
-            ]
-        })
+                },
+            ],
+        });
         const petName = await inquirer.prompt({
             type: 'input',
             name: 'name',
             message: 'What is your pet called?',
         });
-        const age = await inquirer.prompt({
+        const petAge = await inquirer.prompt({
             type: 'input',
             name: 'age',
             message: 'And how old is your pet?',
         });
-        if (typeOfPet == 'cat') {
-            myPet = new Cat(petName,age);
-        }
-        else if (typeOfPet == 'sloth') {
-            myPet = new Sloth(petName,age);
-        }
-        else if (typeOfPet == `dragon`) {
-            myPet = new Dragon(petName,age);
-        }
+        switch(typeOfPet.typeOfPet){
+            case 'cat':
+                myPet = new Cat(petName, petAge, 10, 10, 100);
+                await catFn();
+                await console.log(myPet);
+                break;
+            case 'sloth': 
+                myPet = new Sloth(petName, petAge, 0, 1, 100);
+                break;
+            case 'dragon':
+                myPet = new Dragon(petName, petAge, 0, 100, 0);
+                break;
+            default:
+                break;
 
-        console.log(myPet.name);
-    }
-    start();}
+
+       
+
+            }console.log(myPet);    }
+    start();
+}
     catch(error) {
         console.log("whoops error");
-    }
+    } 
